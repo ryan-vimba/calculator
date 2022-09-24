257bfc50fc3954d48d4b8f0c67f42479d871daf0
ryan-vimba <rvimba@gmail.com>
2022-09-24 13:53:11 -0400

    component for brand labels

---
+++ src/components/Branding.js
@@ -0,0 +1,10 @@
+import './Branding.css';
+
+export const Branding = () => (
+    <div>
+        <text className='ti-branding'>Texas Instruments</text>
+        <text className='model-label'>TI-108</text>
+    </div>
+);
+
+export default Branding;
