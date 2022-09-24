e72bdd994bbb1fbd78369bf43d213527f2933a85
ryan-vimba <rvimba@gmail.com>
2022-09-24 13:53:46 -0400

    component for solar panel

---
+++ src/components/SolarPanel.js
@@ -0,0 +1,11 @@
+import './SolarPanel.css';
+
+export const SolarPanel = () => (
+    <div className='solarPanel'>
+        <div className="verticalLine"/>
+        <div className="verticalLine"/>
+        <div className="verticalLine"/>
+    </div>
+);
+
+export default SolarPanel;
